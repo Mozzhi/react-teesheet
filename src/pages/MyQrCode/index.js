55e1e3937fe7daa9c59ed1fef4876248ccb27151
mozhongzhi <zhongzhi.mo@bgmyc.com>
2018-08-09 18:12:14 +0800

    二维码

--- src/pages/MyQrCode/index.js
+++ src/pages/MyQrCode/index.js
@@ -6,6 +6,9 @@ import QrCodeWithLogo from 'qr-code-with-logo'
 class MyQrCode extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            myMsg: {}
+        }
     }
 
     componentDidMount(){
@@ -16,14 +19,18 @@ class MyQrCode extends Component {
         HttpRequest({
             url:'Users',
             callback: (res) => {
-                console.log(res)
-                const myCanvas = document.createElement('canvas')
-                document.getElementById('qr').appendChild(myCanvas)
+                console.log(res);
+                this.setState({
+                    myMsg: res.data,
+                })
+                const myCanvas = document.createElement('canvas');
+                document.getElementById('qr_show').appendChild(myCanvas);
+                let width = document.documentElement.clientWidth * 300/375;
 
                 QrCodeWithLogo.toCanvas({
                     canvas: myCanvas,
                     content: res.data.tel,
-                    width: 300,
+                    width: width,
                     logo: {
                         src: res.data.user_pic,
                         radius: 8,
@@ -33,10 +40,24 @@ class MyQrCode extends Component {
         })
     }
     render() {
+        const { myMsg } = this.state;
         return (
-            <div id="qr">
-
+            <div className="qr-code">
+                <div id="qr">
+                    <div data-flex="dir:left box:first" className="my-msg">
+                        <div data-flex="main:center cross:center" className="my-pic">
+                            <div style={{backgroundImage: `url(${myMsg.user_pic})`,}}></div>
+                        </div>
+                        <div className="base">
+                            <div className="user-name">{myMsg.player_name}</div>
+                            <div>会籍号:{myMsg.card_num_lang}</div>
+                        </div>
+                    </div>
+                    <div id="qr_show"></div>
+                    <div className="make-sure">扫一扫二维码，快速确认我的信息</div>
+                </div>
             </div>
+
         )
     }
 
